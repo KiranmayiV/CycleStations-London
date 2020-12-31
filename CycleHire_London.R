6a0d500cb46509422d7b599bd5d794b4cbffea09
Kiranmayi Vadlamudi <kiranrox91@gmail.com>
2021-01-01 10:58:16 +1100

    Shiny Application

---
+++ CycleHire_London.R
@@ -0,0 +1,68 @@
+#Shiny app for cycle hire from https://github.com/Robinlovelace/geocompr/issues/584
+# Author - Kiranmayi Vadlamudi
+#25th Dec 2020
+
+library(pacman)
+p_load(sf, shiny, spData, leaflet, tidyverse, spDataLarge)
+
+# Based on input coordinates finding the nearest bicycle points
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Closest available bike in London"),
+  
+  # Sidebar with a slider input for number of bins 
+  fluidRow (
+    column(3,numericInput("x", ("Enter x-coordinate of your location"), value = 51.5000000, step = 0.0000001)),
+    column(3, numericInput("y", ("Enter y-coordinate of your location"), value = -0.1000000 , step = 0.0000001)),
+    column(4, numericInput("num", "How many bikes are you looking for?", value  =1, step  =1))
+    ),
+    
+    # Show a plot of the generated distribution
+    fluidRow(
+      leafletOutput("map", height= 500)
+    )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  #centring the leaflet map onto london
+  map_centre = matrix(c(-0.2574846,51.4948089), nrow=1, ncol=2, dimnames = list(c("r1"), c("X", "Y")))
+  
+  #based on input coords calculating top 5 closest stations to be displayed 
+  input_pt = reactive({matrix(c(input$y, input$x), nrow = 1, ncol=2, dimnames = list(c("r1"), c("X", "Y")))})
+  
+  data = reactive({
+      cycle_hire$dist = st_point(input_pt()) %>%  
+        st_sfc() %>% 
+        st_set_crs(4326) %>% 
+        st_distance(cycle_hire$geometry) %>%
+        t() %>% 
+        set_units(.,"km")
+    
+      cycle_hire[order(cycle_hire$dist),]
+    })
+    
+  filteredData = reactive({
+     filter(data(), nbikes > input$num) %>% head(5)
+
+   })
+    icons = awesomeIcons(icon = "bicycle", library = "fa", squareMarker = TRUE, markerColor = "blue")
+    
+    
+    output$map = renderLeaflet({
+      # Things that do not change go here:
+      filteredData() %>% 
+        mutate(popup = str_c(str_c("Station:", name, sep=" "),
+                                     str_c("Available bikes:", nbikes, sep=" "), sep = "<br/>")) %>% 
+        leaflet() %>% 
+        addTiles() %>%
+        setView(lng = input_pt()[, "X"], input_pt()[, "Y"], zoom = 15) %>% 
+        addAwesomeMarkers(icon = icons, popup = ~popup) %>% 
+        addMarkers(lng = input_pt()[, "X"], input_pt()[, "Y"], label = "Your Location")
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)  
