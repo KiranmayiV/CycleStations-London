a6b70592d6a9c24231a654351a9b7ac408d3a2d6
Kiran <kiranrox91@gmail.com>
2021-01-01 22:30:23 +1100

    app_build

---
+++ AdditionalPackages.R
@@ -0,0 +1,5 @@
+# Installed packages: shiny, tidyverse, devtools
+
+
+install.packages(c('sf','spData', 'leaflet', 'spDataLarge', 'units'), repos = 'http://cran.rstudio.com/')
+install.packages('spDataLarge', repos = "https://nowosad.github.io/drat/", type = "source")
