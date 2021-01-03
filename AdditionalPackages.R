4d39a3946f1db1a6f8f995974c96c4e31f23966c
Kiran <kiranrox91@gmail.com>
2021-01-03 11:29:44 +1100

    removed older filenames

--- AdditionalPackages.R
+++
@@ -1,5 +0,0 @@
-# Installed packages: shiny, tidyverse, devtools
-
-
-install.packages(c('sf','spData', 'leaflet', 'spDataLarge', 'units'), repos = 'http://cran.rstudio.com/')
-install.packages('spDataLarge', repos = "https://nowosad.github.io/drat/", type = "source")
